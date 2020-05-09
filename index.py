acc0fd00ad517a8596c11f4c9bdb2834d41005ac
Regina Galambos <gina.galambos@gmail.com>
2020-05-09 22:38:27 +0200

    update app names

--- index.py
+++ index.py
@@ -4,15 +4,15 @@ import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 
 from app import app, server
-from apps import index, app1, app2, data
+from apps import index, world, countries, data
 
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
     dbc.NavbarSimple(
         children=[
             dbc.NavLink('About', href='/'),
-            dbc.NavLink('World-wide', href='/app1'),
-            dbc.NavLink('Countries', href='/app2'),
+            dbc.NavLink('World-wide', href='/world'),
+            dbc.NavLink('Countries', href='/countries'),
             dbc.NavLink('Data', href='/data'),
         ],
         brand='COVID-19 Project',
@@ -29,10 +29,10 @@ app.layout = html.Div([
 def render_page_content(pathname):
     if pathname == '/':
         return index.layout
-    elif pathname == '/app1':
-        return app1.layout
-    elif pathname == '/app2':
-        return app2.layout
+    elif pathname == '/world':
+        return world.layout
+    elif pathname == '/countries':
+        return countries.layout
     elif pathname == '/data':
         return data.layout
     else:
